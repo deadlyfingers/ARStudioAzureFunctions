1f1d402184706bdde85112584efe9425d724dd1c
David Douglas <david.douglas@microsoft.com>
2018-07-26 17:37:42 +0100

    CosmosDB ssl connection

--- shared/connection.js
+++ shared/connection.js
@@ -10,11 +10,16 @@ const user = process.env.MongoDBUser || '';
 const pass = process.env.MongoDBPass || '';
 const database = process.env.DatabaseName || 'arstudio';
 
+var params = '';
+if (process.env.WEBSITE_NODE_DEFAULT_VERSION) {
+  params = "?ssl=true&replicaSet=globaldb"; // Azure Cosmos DB accounts require secure communication via SSL.
+}
+
 // MongoDB connection
 const options = {};
 const connectionUri = (!user || !pass)
   ? 'mongodb://' + host + '/' + database
-  : util.format('mongodb://%s:%s@%s:%s/%s', user, pass, host, port, database);
+  : util.format('mongodb://%s:%s@%s:%s/%s%s', user, pass, host, port, database, params);
 const connection = mongoose.createConnection(connectionUri, options);
 
 module.exports = {
